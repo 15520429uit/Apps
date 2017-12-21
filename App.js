2861b0bee2a37b4331f301e3f421ca8de6f45488
15520429uit <33143568+15520429uit@users.noreply.github.com>
2017-12-21 15:52:41 +0700

    Add files via upload

--- App.js
+++ App.js
@@ -3,9 +3,33 @@ import { StackNavigator } from 'react-navigation'; // 1.0.0-beta.19
 import ListScreen from './ListScreen.js';
 import DetailScreen from './DetailScreen.js';
 
+ListScreen.navigationOptions = {
+    title: 'List of Items',
+    header: null,
+    headerStyle: {
+      backgroundColor: 'gray',
+    },
+    headerTintColor: 'red',
+    headerTitleStyle: {
+      fontSize: 30,
+      color: '#ffc40c',
+      alignSelf: 'center',
+      fontFamily: 'serif',
+    },
+};
+
+DetailScreen.navigationOptions = {
+    title: 'Item Details',
+    headerTitleStyle: {
+      fontSize: 25,
+      color: 'green',
+      fontFamily: 'serif'
+    },
+};
+
 const SimpleApp = StackNavigator({
-  List: {screen: ListScreen},
-  Detail: {screen: DetailScreen},
+  List: { screen: ListScreen },
+  Detail: { screen: DetailScreen },
 });
 
 export default class App extends React.Component {
